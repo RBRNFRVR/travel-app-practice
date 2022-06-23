64387516587b220f1c3ffd4a303f7d1872b8db0c
RBRNFRVR <rommelchavez99@gmail.com>
2022-06-23 01:42:17 -0400

    list is done

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -6,9 +6,28 @@ import Rating from '@material-ui/lab/Rating';
 import useStyles from './styles'
 
 const PlaceDetails = ({place})=> {
-    console.log(place)
+    const classes = useStyles();
+
     return(
-        <h1>{place.name}</h1>
+       <Card elevation={6}>
+            <CardMedia
+                style={{height: 350}}
+                image={place.photo?place.photo.images.large.url:require('/Users/rbrnfrvr/travel-app/src/images/jason-leung-poI7DelFiVA-unsplash.jpg')}
+                title={place.name}
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5">{place.name?place.name:'Some Place'}</Typography>
+                <Box display="flex" justifyContent="space-between">
+                    <Typography variant="subtitle1">Prices</Typography>
+                    <Typography  gutterBottom variant="subtitle1">{place.price_level?place.price_level:'$'}</Typography>
+                </Box>
+                <Box display="flex" justifyContent="space-between">
+                    <Typography variant="subtitle1">Ranking</Typography>
+                    <Typography  gutterBottom variant="subtitle1">{place.ranking?place.ranking:'some ranking'}</Typography>
+                </Box>
+                
+            </CardContent>
+       </Card>
     );
 }
 
